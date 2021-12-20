ed8f85943725df58f51886743f9fd11f099021b0
Timur <maddhighlights@gmail.com>
2021-12-21 02:25:21 +0600

    Воссоздание видеоплеера

--- video/urls.py
+++ video/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('stream/<int:pk>/', views.get_streaming_video, name='stream'),
     path('<int:pk>/', views.get_video, name='video'),
