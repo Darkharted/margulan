326961c04f2cb89d269f3e4bb55c5344ad9f2f96
Timur <maddhighlights@gmail.com>
2021-12-22 15:53:30 +0600

    покупки билетов для путешествия (Недоработанная)

--- adventure/models.py
+++ adventure/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Adventure(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Куратор", related_name='class_created', on_delete=models.CASCADE)
+
+
