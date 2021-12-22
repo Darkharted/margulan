198207fc5646e68b19ba29b5688bdca3fb162e4f
Timur <maddhighlights@gmail.com>
2021-12-22 15:52:43 +0600

    создание Пакетов для покупки

--- package/serializers.py
+++ package/serializers.py
@@ -5,3 +5,5 @@ from .models import *
 def package_detail(request, id, slug):
     package = get_object_or_404(Package, id=id, slug=slug, available=True)
     return render(request, 'margulan/package/detail.html', {'product': package})
+
+
