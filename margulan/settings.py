69b4833d0a7f1a382173e6f058f94282ffaffe9d
kwantimur <maddhighlights@gmail.com>
2021-12-25 02:09:25 +0600

    added database

--- margulan/settings.py
+++ margulan/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': 'margulan',
         'USER': 'timur',
         'PASSWORD': '1',
-        'HOST': '0.0.0.0',
+        'HOST': 'localhost',
         'PORT': 5432
     }
 }
